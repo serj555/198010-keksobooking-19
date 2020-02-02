520d1293a69d9f77cba0b60a6569b492c5846ece
Трубкин Сергей <trubkinsa@gmail.com>
2020-02-02 14:45:51 +0300

    Исправляет замечания от наставника

--- js/main.js
+++ js/main.js
@@ -10,49 +10,59 @@ var MAP_PIN_TEMPLATE = document.querySelector('#pin')
   .content
   .querySelector('.map__pin');
 var MAP_PINS_BLOCK = document.querySelector('.map__pins');
+var PRICE_MIN = 1000;
+var PRICE_MAX = 5000;
+var ROOM_MIN = 1;
+var ROOM_MAX = 5;
+var GUEST_MIN = 1;
+var GUEST_MAX = 10;
 var TYPES = ['palace', 'flat', 'house', 'bungalo'];
 var CHECK_TIMES = ['12:00', '13:00', '14:00'];
-var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner'];
+var PHOTOS = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 
 // получение ширины DOM-элемента
-var getWidthElement = function (element) {
+var getElementWidth = function (element) {
   return element.offsetWidth;
 };
 
 // определение рандомного числа из диапазона чисел
-var getRandomInteger = function (min, max) {
+var getRandomBetween = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
 // получение значения из рандомного елемента массива
 var getRandomElement = function (array) {
-  return array[Math.floor(Math.random() * (array.length - 1))];
+  return array[getRandomBetween(0, array.length)];
 };
 
 // получение массива случайной длинны из другого массива
 var getRandomNumberElements = function (array) {
-  var numberElements = Math.ceil(Math.random() * array.length);
-  var elements = [];
+  var copyElements = array.slice(0, array.length);
+  var numberElements = getRandomBetween(1, copyElements.length);
+  var takeElements = [];
+  var finalElements = [];
 
   for (var i = 0; i < numberElements; i++) {
-    var randomElement = Math.floor(Math.random() * (numberElements - i)) + i;
-    elements[i] = array[randomElement];
-    array[randomElement] = array[i];
-    array[i] = elements[i];
+    var randomElement = getRandomBetween(0, copyElements.length - 1);
+    takeElements = copyElements.splice(randomElement, 1);
+    finalElements.push(takeElements[0]);
   }
-  return elements;
+  return finalElements;
 };
 
 // получение массива ссылок на аватара пользователя
-var createImageNames = function () {
-  var imageNames = [];
-
-  for (var i = 0; i < NUMBER_USERS; i++) {
-    imageNames[i] = 'img/avatars/user0' + (i + 1) + '.png';
-  }
-
-  return imageNames;
+var createImageNames = function (number) {
+  return 'img/avatars/user0' + (number + 1) + '.png';
 };
 
 // создание массива с данными пользователя
@@ -60,21 +70,21 @@ var createUserData = function (number) {
   var userData = [];
 
   var author = {
-    avatar: createImageNames()[number]
+    avatar: createImageNames(number)
   };
 
   var location = {
-    x: getRandomInteger(MAP_PIN_MIN_X, (getWidthElement(MAP))),
-    y: getRandomInteger(MAP_PIN_MIN_Y, MAP_PIN_MAX_Y)
+    x: getRandomBetween(MAP_PIN_MIN_X, (getElementWidth(MAP))),
+    y: getRandomBetween(MAP_PIN_MIN_Y, MAP_PIN_MAX_Y)
   };
 
   var offer = {
     title: 'заголовок предложения',
     address: location.x + ', ' + location.y,
-    price: 500,
+    price: getRandomBetween(PRICE_MIN, PRICE_MAX),
     type: getRandomElement(TYPES),
-    rooms: 4,
-    guests: 5,
+    rooms: getRandomBetween(ROOM_MIN, ROOM_MAX),
+    guests: getRandomBetween(GUEST_MIN, GUEST_MAX),
     checkin: getRandomElement(CHECK_TIMES),
     checkout: getRandomElement(CHECK_TIMES),
     features: getRandomNumberElements(FEATURES),
